766bdd748c603823ed28ae01f9e553b75cc1d8e6
Mubly <Mubly123@163.com>
2019-03-08 19:24:10 +0800

    地图绘制

--- app/src/main/java/com/mubly/comewaves/view/activity/HotLebelActivity.java
+++ app/src/main/java/com/mubly/comewaves/view/activity/HotLebelActivity.java
@@ -3,6 +3,7 @@ package com.mubly.comewaves.view.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.amap.api.maps2d.MapFragment;
 import com.amap.api.maps2d.MapView;
 import com.mubly.comewaves.R;
 import com.mubly.comewaves.common.base.BasePresenter;
@@ -15,6 +16,7 @@ public class HotLebelActivity extends BaseActivity {
 
     @BindView(R.id.mapView)
     MapView mMapView;
+//    MapFragment textureMapView;
 
     @Override
     protected BasePresenter createPresenter() {
@@ -25,30 +27,35 @@ public class HotLebelActivity extends BaseActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mMapView.onCreate(savedInstanceState);
+
     }
 
     @Override
     protected int getLayoutId() {
         return R.layout.activity_hot_lebel;
     }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         //在activity执行onDestroy时执行mMapView.onDestroy()，销毁地图
         mMapView.onDestroy();
     }
+
     @Override
     protected void onResume() {
         super.onResume();
         //在activity执行onResume时执行mMapView.onResume ()，重新绘制加载地图
         mMapView.onResume();
     }
+
     @Override
     protected void onPause() {
         super.onPause();
         //在activity执行onPause时执行mMapView.onPause ()，暂停地图的绘制
         mMapView.onPause();
     }
+
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
